dae317730b5fd292e664d593065875a103f8d992
Joris <je.tieleman@gmail.com>
2022-11-30 22:36:41 +0100

    finished Questions and comments

--- config/route.js
+++ config/route.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const apps = require('../controller/appFunctions');
+const control = require('../controller/control')
+
 router.get('/', apps.homepage);
 router.get('/auth', apps.authentication);
 router.post('/signUp', apps.registration);
 router.post('/logIn', apps.logIn);
+router.all('/addQuestion', control.postQuestion)
 module.exports = router;
--- controller/control.js
+++ controller/control.js
@@ -7,33 +7,29 @@ const postQuestion = (req, res) => {
         res.render('questionInsertion')
     }
 
-    if (req.method=="POST"){
+    if (req.method == "POST"){
 
-        const newQuestion = {
+        Question.create({
             title: req.body.title,
             description: req.body.description,
             // Get user Id from cookie
-            user_id: user.id
-        }
-        Question.save(newQuestion)
-        .then(res => {
-            message = "Your question has been posted"
-            res.render('homepage', {message})
+            // user_id: user.id
         })
-        .catch(err => res.render('mainPage', {message:err}))
+            .then(result => {
+                console.log('succes')
+                message = "Your question has been posted"
+                res.redirect('/', {message})
+            })
+            .catch(err => res.render('mainPage', {message:err}, console.log('f')))
     }
 }
 
-const viewQuestion = (req, res) => {
-
+const viewQuestion = async (req, res) => {
 
     if (req.method == "GET"){
-        Question.findById({id: req.params.id})
-            .then(result => {
-                
-            })
-            .catch()
-                res.render('questionDetails')
+        question = await Question.findById({_id: req.params.id})
+        comments = await Comments.find({question_id: req.params.id})
+        res.render('questionDetails', {question, comments})
     }
 
     if (req.method == "POST"){
@@ -51,13 +47,35 @@ const viewQuestion = (req, res) => {
                 console.log("comment saved")
                 res.render('homepage', {message})
             })
-            .catch(err =>console.log('comment error'), res.render('mainPage', {message:err}))
+            .catch(err =>res.render('mainPage', {message:err}))
     }
 }
 
-
+editQuestion = (req, res) => {
+    if (req.method == "GET"){
+        Question.findById({_id: req.params.id})
+            .then(result => {
+                render('questionModification', {result})
+            })
+    }
+    if (req.method == "POST"){
+        Question.findByIdAndUpdate({_id: req.params.id})
+            .then(result => {
+                const {question, description} = req.body
+                result = {
+                    Title: question,
+                    Description: description
+                }
+                result.save()
+                    .then(()=> res.redirect(`/question/${req.params.id}`))
+                    .catch(err => console.log(err))
+            })
+            .catch(err => console.log(err))
+    }
+}
 
 module.exports = {
     postQuestion,
-    viewQuestion
+    viewQuestion,
+    editQuestion
 }
