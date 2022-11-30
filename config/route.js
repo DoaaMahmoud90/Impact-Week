dba8d9645132e81fc1bfb35559d92fa69cc3a21d
Doaa Mahmoud <doaa-mahmoud-23@hotmail.com>
2022-11-30 19:11:50 +0100

    improvingAuthentication

--- config/route.js
+++ config/route.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const apps = require('../controller/appFunctions');
 const auth = require('../middleware/authentication');
-router.get('/', apps.homepage);
+router.get('/',auth.checkStatus, apps.homepage);
 router.get('/auth', apps.showAuthenPage);
-router.get('/addQuestion', auth.checkToken,apps.createQuestion)
+router.get('/addQuestion', auth.checkToken,apps.createQuestion);
+router.get('/logOut', apps.loggingOut);
 router.post('/signUp', apps.signUp);
 router.post('/logIn', apps.logIn);
 module.exports = router;
