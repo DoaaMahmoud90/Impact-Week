1dd515816f2b9ccadf0d196116a44a4f9bd16d99
Doaa Mahmoud <doaa-mahmoud-23@hotmail.com>
2022-12-01 17:16:38 +0100

    getting current user info

--- config/route.js
+++ config/route.js
@@ -4,13 +4,13 @@ const apps = require('../controller/appFunctions');
 const control = require('../controller/control')
 const auth = require('../middleware/authentication');
 
-
+router.get('*', auth.checkUser);
 router.get('/', apps.homepage);
 router.get('/auth', apps.showAuthenPage);
 router.get('/logOut', apps.loggingOut);
 router.post('/signUp', apps.signUp);
 router.post('/logIn', apps.logIn);
-router.get('/addQuestion', control.addQuestion)
+router.get('/addQuestion', auth.checkToken, control.addQuestion)
 router.post('/postQuestion', control.postQuestion)
 
 module.exports = router;
--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,36 +1,41 @@
+const { JsonWebTokenError } = require('jsonwebtoken');
 const questionModel = require('../models/questions');
+const userModel = require('../models/user');
+const jwt = require('jsonwebtoken');
 const checkToken = (req,res, next) => {
     if(req.header('cookie')){
       next();
     }
     else{
-    res.redirect('/')
+    res.redirect('/');
     }
 }
 
 
-const checkStatus = (req,res, next) => {
-  if(!req.header('cookie')){
-    next()
-  }
-  else{
-    questionModel.countDocuments({},(err, count) => {
-      if(count == 0)
-      {
-        res.render('mainPage', {
-          message: 'There are no questions',
-          status: "loggedIn"
-        })
-      }else{
-        res.render('mainPage', {
-          message: `There are ${count} questions`,
-          status: "loggedIn"
-        })
-      }
+
+const checkUser = (req,res, next) => {
+  const token = req.cookies.isLoggedIn;
+  if(token){
+    jwt.verify(token, 'secret', async (err, decodedToken) => {
+     if(err){
+      console.log(err.message);
+      res.locals.user = null;
+      next();
+     } else {
+      console.log(decodedToken.id);
+      let user = await userModel.findById(decodedToken.id);
+      res.locals.user = user;
+      console.log(res.locals);
+      next();
+     }
     })
+  } else {
+    res.locals.user = null;
+    next();
   }
 }
 
 
-module.exports = {checkToken, 
-  checkStatus}
+module.exports = 
+  {checkToken, 
+  checkUser}
